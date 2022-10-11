6542411499f9e723afdfad5a568aeafadf82df74
Eric Rosetti Lessa <ericrlessa@gmail.com>
2022-10-11 19:12:30 -0300

    map config utilizando volume

--- server.go
+++ server.go
@@ -1,10 +1,14 @@
 package main
 
-import "net/http"
-import "os"
-import "fmt"
+import ("fmt"
+ "io/ioutil"
+ "log"
+ "net/http"
+ "os"
+)
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
@@ -14,4 +18,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age);
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request){
+	data, err := ioutil.ReadFile("example/example.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err);
+	}
+	fmt.Fprintf(w, "Successful: %s.", string(data))
 }
