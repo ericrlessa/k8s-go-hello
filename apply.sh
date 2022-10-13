98e58dff1493373587f5a5772a9e5c91cbfcfa2e
Eric Rosetti Lessa <ericrlessa@gmail.com>
2022-10-13 16:47:41 -0300

    shell script utilitario e deployment com resources

---
+++ apply.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+kubectl apply -f config/service/service-loadbalance.yaml
+kubectl apply -f config/deployment/secret.yaml
+kubectl apply -f config/deployment/config-map/configmap-env.yaml
+kubectl apply -f config/deployment/config-map/configmap-file.yaml
+kubectl apply -f config/metrics-server.yaml
