0fffc7b58bdaf97ae7961764944ffda03e11f52b
Eric Rosetti Lessa <ericrlessa@gmail.com>
2022-10-14 09:44:46 -0300

    script auxiliar

--- apply.sh
+++ apply.sh
@@ -5,3 +5,4 @@ kubectl apply -f config/deployment/secret.yaml
 kubectl apply -f config/deployment/config-map/configmap-env.yaml
 kubectl apply -f config/deployment/config-map/configmap-file.yaml
 kubectl apply -f config/metrics-server.yaml
+kubectl apply -f config/pvc.yaml
